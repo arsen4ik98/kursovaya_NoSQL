2acb3a6403e55f0a1bbc59f022ccbd8197fa43e0
arsen4ik98 <32158122+arsen4ik98@users.noreply.github.com>
2020-05-18 13:31:13 +0300

    Вывел данные с MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectID;
-   
+
 const app = express();
 const jsonParser = express.json();
-const mongoClient = new MongoClient("mongodb://localhost:27017/test", { useNewUrlParser: true });
+   
+const url = "mongodb://localhost:27017/";
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+ 
 let dbClient;
-app.use(express.static("public"));
+
+
+app.use(express.static(__dirname + "/public"));
 
 mongoClient.connect(function(err, client){
     if(err) return console.log(err);
-  
-    
     dbClient = client;
     app.locals.collection = client.db("test").collection("Routen_avto");
     app.listen(3000, function(){
@@ -19,16 +22,17 @@ mongoClient.connect(function(err, client){
     });
 });
 
-app.get("/test", function(req, res){
+app.get("/Routen_avto", function(req, res){
         
     const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, Routen_avto){
+    collection.find({}).toArray(function(err, users){
          
         if(err) return console.log(err);
-        res.send(Routen_avto)
+        res.send(users)
     });
      
 });
+
 // прослушиваем прерывание работы программы (ctrl-c)
 process.on("SIGINT", () => {
     dbClient.close();
