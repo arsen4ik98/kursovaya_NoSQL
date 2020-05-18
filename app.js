1e00e448584495591062f9daaa61db31367e8614
arsen4ik98 <32158122+arsen4ik98@users.noreply.github.com>
2020-05-18 15:14:01 +0300

    Вывел все 3 коллекций

--- app.js
+++ app.js
@@ -16,13 +16,16 @@ app.use(express.static(__dirname + "/public"));
 mongoClient.connect(function(err, client){
     if(err) return console.log(err);
     dbClient = client;
-    app.locals.collection = client.db("test").collection("Routen_avto");
+    app.locals.collection = client.db("test").collection("Routen_Trol");
+    app.locals.collection2 = client.db("test").collection("Routen_Avto");
+    app.locals.collection3 = client.db("test").collection("Routen_Tram");
+    
     app.listen(3000, function(){
         console.log("Сервер ожидает подключения...");
     });
 });
 
-app.get("/Routen_avto", function(req, res){
+app.get("/Routen_Trol", function(req, res){
         
     const collection = req.app.locals.collection;
     collection.find({}).toArray(function(err, users){
@@ -32,6 +35,27 @@ app.get("/Routen_avto", function(req, res){
     });
      
 });
+app.get("/Routen_Avto", function(req, res){
+        
+    const collection = req.app.locals.collection2;
+    collection.find({}).toArray(function(err, users){
+         
+        if(err) return console.log(err);
+        res.send(users)
+    });
+     
+});
+app.get("/Routen_Tram", function(req, res){
+        
+    const collection = req.app.locals.collection3;
+    collection.find({}).toArray(function(err, users){
+         
+        if(err) return console.log(err);
+        res.send(users)
+    });
+     
+});
+
 
 // прослушиваем прерывание работы программы (ctrl-c)
 process.on("SIGINT", () => {
